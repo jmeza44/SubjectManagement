b2668fe5832098d0a1729611f327462aa272cdad
Jaime Meza <jaimealbertomeza@hotmail.com>
2022-12-17 00:15:53 -0500

    AppDbContext and SubjectRepository added on the service container

--- SubjectsManagement.WebApi/Program.cs
+++ SubjectsManagement.WebApi/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SubjectsManagement.Domain.Abstractions;
+using SubjectsManagement.Persistence;
+using SubjectsManagement.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+var ConnectionString = builder.Configuration.GetConnectionString("Default");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(ConnectionString));
+builder.Services.AddScoped<ISubjectRepository, SubjectRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
